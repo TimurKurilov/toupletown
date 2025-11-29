f56644aed8a60dbd0bfadd9653982dd1fac34a18
TimurKurilov <kyrilov.timur@gmail.com>
2025-11-29 14:08:53 +0300

    worked fact cache

--- toupletown/quote/views.py
+++ toupletown/quote/views.py
@@ -10,7 +10,7 @@ from .models import CityRawData
 from django.core.cache import cache
 
     
-def fetch_city_data(city_name, country_name):
+def fetch_city_data(request, city_name=None, country_name=None):
         wiki_wiki = wikipediaapi.Wikipedia(user_agent='TestProject (test@gmail.com)', language='en')
         load_dotenv()
         GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
@@ -18,12 +18,12 @@ def fetch_city_data(city_name, country_name):
         client = genai.Client(api_key=GEMINI_API_KEY)
         if "geo" not in request.session:
             r = requests.get(f"https://api.2ip.io/?token={TOKEN}")
-            requests.request.session["geo"] = r.json()
-        geo = requests.request.session["geo"]
-        city_name = geo["city"]
-        country_name = geo["country"]
-        
-        
+            request.session["geo"] = r.json()
+        geo = request.session["geo"]
+        city_name = geo.get("city", "Unknown")
+        country_name = geo.get("country", "Unknown")
+
+
         page = wiki_wiki.page(city_name)
         if page.exists():
             summary = '. '.join(page.summary.split('. ')[:5])
@@ -46,29 +46,30 @@ def fetch_city_data(city_name, country_name):
 
         if results["results"]["bindings"]:
             res = results["results"]["bindings"][0]
-            x = {
-                "city": city_name,
-                "country": country_name,
-                "population": res.get("population", {}).get("value"),
-                "info": summary
-            }
-        data = fetch_city_data(country_name, city_name)
+            population = res.get("population", {}).get("value")
+        data = {
+        "city": city_name,
+        "country": country_name,
+        "population": population,
+        "info": summary,
+        "wikidata_raw": summary_raw
+    }
         data_json = json.dumps(data, ensure_ascii=False)
-
         obj, created = CityRawData.objects.get_or_create(
         city=city_name,
         defaults={
             'country': country_name,
             'info': summary,
             'raw': {
-                'population': x[population],
-                'founded': x[founded],
+                'population': population,
+                'founded': None,
                 'wikidata_raw': summary_raw,
             }
         }
     )   
-        if cache.get(f"{city_name}:{country_name}"):
-            return render("quote/main.html", {"text": r.get(f"{city_name}:{text}").decode("utf-8"), "data_json": data_json, "citygpt": city_name, "countrygpt": country_name})
+        cachee = cache.get(f"{city_name}:{country_name}")
+        if cachee:
+            return render(request, "quote/main.html", {"text": cachee, "data_json": data_json, "citygpt": city_name, "countrygpt": country_name})
         else:
             prompt = (
                 "Ты — генератор кратких фактов о городах. "
@@ -89,6 +90,6 @@ def fetch_city_data(city_name, country_name):
             except:
                 text = response.candidates[0].content.parts[0].text
                 
-            cache.set(f"{city_name}:{text}", timeout=259200)
+            cache.set(f"{city_name}:{country_name}", text, timeout=259200)
 
-        return render(request, "quote/main.html", {"text": r.get(f"{city_name}:{text}").decode("utf-8"), "data_json": data_json, "citygpt": city_name, "countrygpt": country_name})
+        return render(request, "quote/main.html", {"text": text, "data_json": data_json, "citygpt": city_name, "countrygpt": country_name})
